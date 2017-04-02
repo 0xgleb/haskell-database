dd803dd16bf194bd16f19df70a28de9eede59c71
Gleb Dianov <uti55531@gmail.com>
2017-04-02 15:45:11 +0100

    ToByteString PolyType

--- Parser/PolyType.hs
+++ Parser/PolyType.hs
@@ -5,13 +5,22 @@ module Parser.PolyType
 , readSomething
 ) where
 
-data PolyType = PolyString String | PolyFloat Float | PolyInt Int | Invalid
+import Data.ByteString.Conversion
+
+data PolyType = PolyString String | PolyFloat Float | PolyInt Int | PolyBool Bool | Invalid
     deriving (Eq, Ord)
+
 instance Show PolyType where
     show (PolyString str) = show str
     show (PolyFloat int) = show int
     show (PolyInt float) = show float
     show Invalid = "Invalid"
+
+instance ToByteString PolyType where
+    builder (PolyString str) = builder str
+    builder (PolyFloat int) = builder int
+    builder (PolyInt float) = builder float
+    builder Invalid = mempty
 
 polyRead :: String -> String -> PolyType
 polyRead "Int" str = case reads str :: [(Int,String)] of
@@ -22,6 +31,9 @@ polyRead "Float" str = case reads str :: [(Float,String)] of
                             _         -> Invalid
 polyRead "String" str = case reads str :: [(String,String)] of
                             [(x, "")] -> PolyString x
+                            _         -> Invalid
+polyRead "Bool" str = case reads str :: [(Bool,String)] of
+                            [(x, "")] -> PolyBool x
                             _         -> Invalid
 polyRead _ _ = Invalid
 
