aaef270f8730520ced560b57a9c1288d2e05a1d5
Gleb Dianov <uti55531@gmail.com>
2017-05-21 22:37:17 +0100

    made some progress with the update procedure

--- src/cbits/file_methods.c
+++ src/cbits/file_methods.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-int update(char *file_name, int byte, int len, uint8_t *data) {
+#define CANNOT_OPEN_FILE_ERROR 1
+#define CANNOT_WRITE_ALL_DATA 2
+#define CANNOT_READ_ALL_DATA 3
+#define SUCCESS 0
+
+int update(char *file_name, long byte, size_t len, uint8_t *data) {
     FILE *file_ptr;
 
-    file_ptr = fopen(file_name, "wr");
+    file_ptr = fopen(file_name, "wrb");
 
     if(file_ptr) {
+			fseek(file_ptr, byte, SEEK_SET);
+			uint8_t *backup = (uint8_t*) malloc(len);
+			fread(backup, 1, len, file_ptr);
+
+			size_t bytes_written = fwrite(data, 1, len, file_ptr);
+
+			if(bytes_written != len) {
+				fwrite(backup, 1, bytes_written, file_ptr);
+				fclose(file_ptr);
+
+				return CANNOT_WRITE_ALL_DATA;
+			}
     }
     else {
-        printf("Unable to open the file!");
-        return 1;
+        printf("Unable to open the file %s!\n", file_name);
+        return CANNOT_OPEN_FILE_ERROR;
     }
 
     fclose(file_ptr);
 
-    return 0; 
+    return SUCCESS; 
 }
 
 int delete(char *file_name, int byte, int len) { 
